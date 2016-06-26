791246a42da6498bf2c8a74f3e255cb370c1fe6a
Himanshu Gupta <himanshu@knoldus.com>
2016-06-26 12:50:41 +0530

    Comparison operators in JavaScript
    Created a sample of Comparison operators in JavaScript.

---
+++ Operators/operator3.js
@@ -0,0 +1,17 @@
+var a = true;
+var b = false;
+var linebreak = "<br />";
+document.write("(a && b) => ");
+result = (a && b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a || b) => ");
+result = (a || b);
+document.write(result);
+document.write(linebreak);
+
+document.write("!(a && b) => ");
+result = (!(a && b));
+document.write(result);
+document.write(linebreak);
