d6b5fcfb3ce5cb50c8e876629485c7581c52e668
Himanshu Gupta <himanshu@knoldus.com>
2016-07-23 12:22:35 +0530

    added sample of toString() method of number

---
+++ Number/string.js
@@ -0,0 +1,9 @@
+var num = new Number(15);
+document.write("num.toString() is " + num.toString());
+document.write("<br />");
+
+document.write("num.toString(2) is " + num.toString(2));
+document.write("<br />");
+
+document.write("num.toString(4) is " + num.toString(4));
+document.write("<br />");
